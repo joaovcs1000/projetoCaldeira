676b10d3c7843dfbb0be48c82438a3a4c2d03ae5
joaovcs1000 <127844134+joaovcs1000@users.noreply.github.com>
2023-06-26 13:18:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,143 @@
+const botao1 = document.getElementById("botoes1")
+const botao2 = document.getElementById("botoes2")
+const botao3 = document.getElementById("botoes3")
+const botao4 = document.getElementById("botoes4")
+const botao5 = document.getElementById("botoes5")
+const botao6 = document.getElementById("botoes6")
+const botao7 = document.getElementById("botoes7")
+const botao8 = document.getElementById("botoes8")
+const botao9 = document.getElementById("botoes9")
+const botao10 = document.getElementById("botoes10")
+const botao11 = document.getElementById("botoes11")
+const botao12 = document.getElementById("botoes12")
+const botao13 = document.getElementById("botoes13")
+const botao14 = document.getElementById("botoes14")
+const botao15 = document.getElementById("botoes15")
+const botao16 = document.getElementById("botoes16")
+const botao17 = document.getElementById("botoes17")
+const botao18 = document.getElementById("botoes18")
+const botao19 = document.getElementById("botoes19")
+const botao20 = document.getElementById("botoes20")
+
+botao1.addEventListener("click", ()=>{
+    botao1.innerHTML = "✅"
+})
+botao2.addEventListener("click", ()=>{
+    botao2.innerHTML = "✅"
+})
+botao3.addEventListener("click", ()=>{
+    botao3.innerHTML = "✅"
+})
+botao4.addEventListener("click", ()=>{
+    botao4.innerHTML = "✅"
+})
+botao5.addEventListener("click", ()=>{
+    botao5.innerHTML = "✅"
+})
+botao6.addEventListener("click", ()=>{
+    botao6.innerHTML = "✅"
+})
+botao7.addEventListener("click", ()=>{
+    botao7.innerHTML = "✅"
+})
+botao8.addEventListener("click", ()=>{
+    botao8.innerHTML = "✅"
+})
+botao9.addEventListener("click", ()=>{
+    botao9.innerHTML = "✅"
+})
+botao10.addEventListener("click", ()=>{
+    botao10.innerHTML = "✅"
+})
+botao11.addEventListener("click", ()=>{
+    botao11.innerHTML = "✅"
+})
+botao12.addEventListener("click", ()=>{
+    botao12.innerHTML = "✅"
+})
+botao13.addEventListener("click", ()=>{
+    botao13.innerHTML = "✅"
+})
+botao14.addEventListener("click", ()=>{
+    botao14.innerHTML = "✅"
+})
+botao15.addEventListener("click", ()=>{
+    botao15.innerHTML = "✅"
+})
+botao16.addEventListener("click", ()=>{
+    botao16.innerHTML = "✅"
+})
+botao17.addEventListener("click", ()=>{
+    botao17.innerHTML = "✅"
+})
+botao18.addEventListener("click", ()=>{
+    botao18.innerHTML = "✅"
+})
+botao19.addEventListener("click", ()=>{
+    botao19.innerHTML = "✅"
+})
+botao20.addEventListener("click", ()=>{
+    botao20.innerHTML = "✅"
+})
+//////////////////////////////////////////////////
+botao1.addEventListener("dblclick", ()=>{
+    botao1.innerHTML = ""
+})
+botao2.addEventListener("dblclick", ()=>{
+    botao2.innerHTML = ""
+})
+botao3.addEventListener("dblclick", ()=>{
+    botao3.innerHTML = ""
+})
+botao4.addEventListener("dblclick", ()=>{
+    botao4.innerHTML = ""
+})
+botao5.addEventListener("dblclick", ()=>{
+    botao5.innerHTML = ""
+})
+botao6.addEventListener("dblclick", ()=>{
+    botao6.innerHTML = ""
+})
+botao7.addEventListener("dblclick", ()=>{
+    botao7.innerHTML = ""
+})
+botao8.addEventListener("dblclick", ()=>{
+    botao8.innerHTML = ""
+})
+botao9.addEventListener("dblclick", ()=>{
+    botao9.innerHTML = ""
+})
+botao10.addEventListener("dblclick", ()=>{
+    botao10.innerHTML = ""
+})
+botao11.addEventListener("dblclick", ()=>{
+    botao11.innerHTML = ""
+})
+botao12.addEventListener("dblclick", ()=>{
+    botao12.innerHTML = ""
+})
+botao13.addEventListener("dblclick", ()=>{
+    botao13.innerHTML = ""
+})
+botao14.addEventListener("dblclick", ()=>{
+    botao14.innerHTML = ""
+})
+botao15.addEventListener("dblclick", ()=>{
+    botao15.innerHTML = ""
+})
+botao16.addEventListener("dblclick", ()=>{
+    botao16.innerHTML = ""
+})
+botao17.addEventListener("dblclick", ()=>{
+    botao17.innerHTML = ""
+})
+botao18.addEventListener("dblclick", ()=>{
+    botao18.innerHTML = ""
+})
+botao19.addEventListener("dblclick", ()=>{
+    botao19.innerHTML = ""
+})
+botao20.addEventListener("dblclick", ()=>{
+    botao20.innerHTML = ""
+})
+////////////////////////////////////////////
